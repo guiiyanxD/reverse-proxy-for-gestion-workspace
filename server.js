1eaad132854e9a5a04f349001339f36b7348176c
Will Victus <williamswaz18@gmail.com>
2026-06-02 23:10:12 -0400

    adding cros

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const {createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use('/api', createProxyMiddleware ({
 	target: 'http://uhqu8rqq4t33xe1c0012wvrb.155.133.27.31.sslip.io',
-	changeOrigin: true
+	changeOrigin: true,
+	onProxyReq: (proxyReq, req, res) => {
+        if (req.method === 'OPTIONS') {
+            res.status(200).send();
+        }
+    }
 }));
 
 app.listen(3000, ()=>console.log('Proxy corriento en el puerto 3000'));
